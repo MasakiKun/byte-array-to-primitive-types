fc17eee071eaa692bc1223fa58c74c454a5c8a90
MasakiKun <kidconan@gmail.com>
2022-08-12 18:15:48 +0900

    static 메서드들 작성

---
+++ src/main/java/kr/ayukawa/ByteUtils.java
@@ -0,0 +1,181 @@
+package kr.ayukawa;
+
+public class ByteUtils {
+	// primitive types to byte array
+
+	/**
+	 * byte를 byte array로 변환한다
+	 * @param data
+	 * @return
+	 */
+	public static byte[] toByteArrays(byte data) {
+		return new byte[] { data };
+	}
+
+	/**
+	 * short를 byte array로 변환한다
+	 * @param data
+	 * @return
+	 */
+	public static byte[] toByteArrays(short data) {
+		return new byte[] {
+				(byte)((data >> 8) & 0xff),
+				(byte)((data >> 0) & 0xff)
+		};
+	}
+
+	/**
+	 * char를 byte array로 변환한다
+	 * @param data
+	 * @return
+	 */
+	public static byte[] toByteArrays(char data) {
+		return new byte[] {
+				(byte)((data >> 8) & 0xff),
+				(byte)((data >> 0) & 0xff)
+		};
+	}
+
+	/**
+	 * int를 byte array로 변환한다
+	 * @param data
+	 * @return
+	 */
+	public static byte[] toByteArrays(int data) {
+		return new byte[] {
+				(byte)((data >> 24) & 0xff),
+				(byte)((data >> 16) & 0xff),
+				(byte)((data >> 8) & 0xff),
+				(byte)((data >> 0) & 0xff),
+		};
+	}
+
+	/**
+	 * long을 byte array로 변환한다
+	 * @param data
+	 * @return
+	 */
+	public static byte[] toByteArrays(long data) {
+		return new byte[] {
+				(byte)((data >> 56) & 0xff),
+				(byte)((data >> 48) & 0xff),
+				(byte)((data >> 40) & 0xff),
+				(byte)((data >> 32) & 0xff),
+				(byte)((data >> 24) & 0xff),
+				(byte)((data >> 16) & 0xff),
+				(byte)((data >> 8) & 0xff),
+				(byte)((data >> 0) & 0xff),
+		};
+	}
+
+	/**
+	 * float을 byte array로 변환한다
+	 * @param data
+	 * @return
+	 */
+	public static byte[] toByteArrays(float data) {
+		return toByteArrays(Float.floatToRawIntBits(data));
+	}
+
+	/**
+	 * double을 byte array로 변환한다
+	 * @param data
+	 * @return
+	 */
+	public static byte[] toByteArrays(double data) {
+		return toByteArrays(Double.doubleToRawLongBits(data));
+	}
+
+	// byte array to primitive type
+
+	/**
+	 * byte array를 byte로 변환한다. 넘겨받은 byte array가 null이거나, length가 1 미만일 경우 IllegalArgumentException이 발생한다
+	 * @param data
+	 * @return
+	 * 전달받은 배열의 0번째 값을 반환한다
+	 */
+	public static byte toByte(byte[] data) {
+		if(data == null || data.length < 1) throw new IllegalArgumentException("given byte array is null or length less than 1");
+
+		return data[0];
+	}
+
+	/**
+	 * byte array를 short로 변환한다. 넘겨받은 byte array가 null이거나, length가 2 미만일 경우 IllegalArgumentException이 발생한다
+	 * @param data
+	 * @return
+	 */
+	public static short toShort(byte[] data) {
+		if(data == null || data.length < 2) throw new IllegalArgumentException("given byte array is null or length less than 2");
+
+		return (short)(
+				(0xff & data[0]) << 8 |
+				(0xff & data[1]) << 0
+		);
+	}
+
+	/**
+	 * byte array를 char로 변환한다. 넘겨받은 byte array가 null이거나, length가 2 미만일 경우 IllegalArgumentException이 발생한다
+	 * @param data
+	 * @return
+	 */
+	public static char toChar(byte[] data) {
+		if(data == null || data.length < 2) throw new IllegalArgumentException("given byte array is null or length less then 2");
+
+		return (char)(
+				(0xff & data[0]) << 8 |
+				(0xff & data[1]) << 0
+		);
+	}
+
+	/**
+	 * byte array를 int로 변환한다. 넘겨받은 byte array가 null이거나, length가 4 미만일 경우 IllegalArgumentException이 발생한다
+	 * @param data
+	 * @return
+	 */
+	public static int toInt(byte[] data) {
+		if(data == null || data.length < 4) throw new IllegalArgumentException("given byte array is null or length less than 4");
+
+		return (int)(
+				(0xff & data[0]) << 24 |
+				(0xff & data[1]) << 16 |
+				(0xff & data[2]) << 8 |
+				(0xff & data[3]) << 0
+		);
+	}
+
+	/**
+	 * byte array를 long으로 변환한다. 넘겨받은 byte array가 null이거나, length가 8 미만이라면 IllegalArgumentException이 발생한다
+	 * @param data
+	 * @return
+	 */
+	public static long toLong(byte[] data) {
+		if(data == null || data.length < 8) throw new IllegalArgumentException("given byte array is null or length less then 8");
+
+		return (long)(
+				(long)(0xff & data[0]) << 56 |
+				(long)(0xff & data[1]) << 48 |
+				(long)(0xff & data[2]) << 40 |
+				(long)(0xff & data[3]) << 32 |
+				(long)(0xff & data[4]) << 24 |
+				(long)(0xff & data[5]) << 16 |
+				(long)(0xff & data[6]) << 8 |
+				(long)(0xff & data[7])
+		);
+	}
+
+	/**
+	 * byte array를 float으로 변환한다. 넘겨받은 byte array가 null이거나 length가 8 미만이라면 IllegalArgumentException이 발생한다
+	 * @param data
+	 * @return
+	 */
+	public static float toFloat(byte[] data) {
+		if(data == null || data.length < 4) throw new IllegalArgumentException("given byte array is null or length less then 4");
+		return Float.intBitsToFloat(toInt(data));
+	}
+
+	public static double toDouble(byte[] data) {
+		if(data == null || data.length < 8) throw new IllegalArgumentException("given byte array is null or length less then 8");
+		return Double.longBitsToDouble(toLong(data));
+	}
+}
---
+++ src/test/java/kr/ayukawa/ByteUtilsTest.java
@@ -0,0 +1,277 @@
+package kr.ayukawa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ByteUtilsTest {
+
+	@Test
+	void byte를_바이트배열로_변환() {
+		byte data = 0b00001111;
+
+		byte[] result = ByteUtils.toByteArrays(data);
+
+		assertEquals(result.length, 1);
+		assertEquals(result[0], data);
+	}
+
+	@Test
+	void short를_바이트배열로_변환() {
+		short data = 0b0000_1000_0000_1111;
+
+		byte[] result = ByteUtils.toByteArrays(data);
+
+		assertEquals(result.length, 2);
+		assertEquals(result[0], 0b00001000);
+		assertEquals(result[1], 0b00001111);
+	}
+
+	@Test
+	void char를_바이트배열로_변환() {
+		// 'a' == 97 == 0b_0000_0000_0110_0001
+		char data = 'a';
+
+		byte[] result = ByteUtils.toByteArrays(data);
+
+		assertEquals(result.length, 2);
+		assertEquals(result[0], 0b00000000);
+		assertEquals(result[1], 0b01100001);
+	}
+
+	@Test
+	void int를_바이트배열로_변환() {
+		int data = 0b0111_1111_0011_0111_0001_0011_0111_1111;
+
+		byte[] result = ByteUtils.toByteArrays(data);
+
+		assertEquals(result.length, 4);
+		assertEquals(result[0], 0b01111111);
+		assertEquals(result[1], 0b00110111);
+		assertEquals(result[2], 0b00010011);
+		assertEquals(result[3], 0b01111111);
+	}
+
+	@Test
+	void long을_바이트배열로_변환() {
+		long data = 0b0111_1111___0011_1111___0001_0111___0011_1111___0111_0101___0011_1001___0111_1100____0001_0001L;
+
+		byte[] result = ByteUtils.toByteArrays(data);
+
+		assertEquals(result.length, 8);
+		assertEquals(result[0], 0b01111111);
+		assertEquals(result[1], 0b00111111);
+		assertEquals(result[2], 0b00010111);
+		assertEquals(result[3], 0b00111111);
+		assertEquals(result[4], 0b01110101);
+		assertEquals(result[5], 0b00111001);
+		assertEquals(result[6], 0b01111100);
+		assertEquals(result[7], 0b00010001);
+	}
+
+	@Test
+	void float를_바이트배열로_변환() {
+		float data = Float.intBitsToFloat(0b0111_1111___0011_1111___0001_1111___0000_1111);
+// ___0000_0111___0000_0011___0000_0001___0000_0000L
+		byte[] result = ByteUtils.toByteArrays(data);
+
+		assertEquals(result.length, 4);
+		assertEquals(result[0], 0b01111111);
+		assertEquals(result[1], 0b00111111);
+		assertEquals(result[2], 0b00011111);
+		assertEquals(result[3], 0b00001111);
+	}
+
+	@Test
+	void double을_바이트배열로_변환() {
+		double data = Double.longBitsToDouble(0b0111_1111___0011_1111___0001_1111___0000_1111___0000_0111___0000_0011___0000_0001___0000_0000L);
+
+		byte[] result = ByteUtils.toByteArrays(data);
+
+		assertEquals(result.length, 8);
+		assertEquals(result[0], 0b01111111);
+		assertEquals(result[1], 0b00111111);
+		assertEquals(result[2], 0b00011111);
+		assertEquals(result[3], 0b00001111);
+		assertEquals(result[4], 0b00000111);
+		assertEquals(result[5], 0b00000011);
+		assertEquals(result[6], 0b00000001);
+		assertEquals(result[7], 0b00000000);
+	}
+
+	@Test
+	void byte배열을_byte로_변환시_null이_전달되면_예외() {
+		try {
+			ByteUtils.toByte(null);
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_byte로_변환시_length가_1미만이면_예외() {
+		try {
+			byte[] data = new byte[] {};
+			ByteUtils.toByte(data);
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_byte로_변환() {
+		byte[] data = new byte[] { 0x50, 0x51, 0x52, 0x53 };
+
+		byte result = ByteUtils.toByte(data);
+
+		assertEquals(result, 0x50);
+	}
+
+	@Test
+	void byte배열을_char로_변환시_null이_전달되면_예외() {
+		try {
+			ByteUtils.toChar(null);
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_char로_변환시_length가_2미만이면_예외() {
+		try {
+			ByteUtils.toChar(new byte[] {});
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_char로_변환() {
+		byte[] data = new byte[] {
+				0b0000_0000, 0b0100_0001
+		};
+		char result = ByteUtils.toChar(data);
+		assertEquals(result, 'A');
+	}
+
+	@Test
+	void byte배열을_short로_변환시_null이_전달되면_예외() {
+		try {
+			ByteUtils.toShort(null);
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_short로_변환시_length가_2미만이면_예외() {
+		try {
+			byte[] data = new byte[]{};
+			ByteUtils.toShort(data);
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_short로_변환() {
+		byte[] data = new byte[] { (byte)0b0111_1111, (byte)0b1111_1111 };
+		short result = ByteUtils.toShort(data);
+
+		assertEquals(result, Short.MAX_VALUE);
+	}
+
+	@Test
+	void byte배열을_int로_변환시_null이_전달되면_예외() {
+		try {
+			ByteUtils.toInt(null);
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_int로_변환시_length가_4미만이면_예외() {
+		try {
+			ByteUtils.toInt(new byte[] {});
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_int로_변환() {
+		byte[] data = new byte[] { (byte)0b0111_1111, (byte)0xff, (byte)0xff, (byte)0xff};
+		int result = ByteUtils.toInt(data);
+		assertEquals(result, Integer.MAX_VALUE);
+	}
+
+	@Test
+	void byte배열을_long으로_변환시_null이_전달되면_예외() {
+		try {
+			ByteUtils.toLong(null);
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_long_으로_변환시_length가_8미만이면_예외() {
+		try {
+			ByteUtils.toLong(new byte[] {});
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_long으로_변환() {
+		byte[] data = new byte[] {
+				0b0111_1111, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff
+		};
+
+		long result = ByteUtils.toLong(data);
+
+		assertEquals(result, Long.MAX_VALUE);
+	}
+
+	@Test
+	void byte배열을_float으로_변환시_null이_전달되면_예외() {
+		try {
+			ByteUtils.toFloat(null);
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_float으로_변환시_length가_4미만이면_예외() {
+		try {
+			ByteUtils.toFloat(new byte[] {});
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_float으로_변환() {
+		byte[] data = new byte[] {
+			0b0111_1111, (byte)0xff, (byte)0xff, (byte)0xff
+		};
+		float result = ByteUtils.toFloat(data);
+		assertEquals(result, Float.intBitsToFloat(Integer.MAX_VALUE));
+	}
+
+	@Test
+	void byte배열을_double로_변환시_null이_전달되면_예외() {
+		try {
+			ByteUtils.toDouble(null);
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_double로_변환시_length가_8미만이면_예외() {
+		try {
+			ByteUtils.toDouble(new byte[] {});
+			fail();
+		} catch(IllegalArgumentException e) {}
+	}
+
+	@Test
+	void byte배열을_double로_변환() {
+		byte[] data = new byte[] {
+				0b0111_1111, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff
+		};
+		double result = ByteUtils.toDouble(data);
+		assertEquals(result, Double.longBitsToDouble(Long.MAX_VALUE));
+	}
+}
